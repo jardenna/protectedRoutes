e639aceac4bf9c50041c14026e165f5db2c72a3b
jardenna <hbjornum@gmail.com>
2024-06-04 17:46:52 +0200

    lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import './scss/index.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './scss/index.scss';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container);
