afc0703b93dc791baceaa13b62e3262370ee960e
jardenna <hbjornum@gmail.com>
2024-06-05 09:31:14 +0200

    layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,25 +1,24 @@
+import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import AuthStatus from '../components/AuthStatus';
 import Nav from '../components/Nav';
 
-function Layout() {
-  return (
-    <section className="main-container">
-      <header className="header">
-        <article className="container">
-          <div className="logo">Auth Example using RouterProvider</div>
-          <div>
-            <Nav />
-          </div>
-          <AuthStatus />
-        </article>
-      </header>
-      <main className="container">
-        <Outlet />
-      </main>
-      <footer>Footer</footer>
-    </section>
-  );
-}
+const Layout: FC = () => (
+  <section className="main-container">
+    <header className="header">
+      <article className="container">
+        <div className="logo">Auth Example using RouterProvider</div>
+        <div>
+          <Nav />
+        </div>
+        <AuthStatus />
+      </article>
+    </header>
+    <main className="container">
+      <Outlet />
+    </main>
+    <footer>Footer</footer>
+  </section>
+);
 
 export default Layout;
